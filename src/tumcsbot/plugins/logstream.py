c86ec1fe5802f7d669562b56fb49780a4be02b7e
joshy <janez.rotman@tum.de>
2024-08-08 15:38:59 +0200

    cleanup logstream

--- src/tumcsbot/plugins/logstream.py
+++ src/tumcsbot/plugins/logstream.py
@@ -3,33 +3,11 @@
 # See LICENSE file for copyright and license details.
 # TUM CS Bot - https://github.com/ro-i/tumcsbot
 
-"""Manage reactions on certain words or phrases with emojis.
-
-Use the Zulip facility, see https://zulip.com/help/add-an-alert-word.
-Provide also an interactive command so administrators are able to
-change the alert words and specify the emojis to use for the reactions.
-"""
-
-# TODO: replacement for zulip usergroups. Replace as soon as api allows bot requests for usergroups
-
 import logging
-from typing import Any, AsyncGenerator
-from sqlalchemy import Column, Integer, String, ForeignKey
-import sqlalchemy
-from sqlalchemy.orm import relationship, Mapped
-from sqlalchemy.ext.hybrid import hybrid_property
-import yaml
-from zulip import ZulipStream
-
 from tumcsbot.lib.client import AsyncClient
-from tumcsbot.lib.command_parser import CommandParser
 from tumcsbot.lib.conf import Conf
-from tumcsbot.lib.db import Session, TableBase, serialize_model
 from tumcsbot.lib.response import Response
-from tumcsbot.lib.types import DMMessage, DMResponse
 from tumcsbot.plugin import PluginCommandMixin, Plugin
-from tumcsbot.plugin_decorators import command
-from tumcsbot.plugins.userinput import UserInput
 
 
 class ZulipLogHandler(logging.Handler):
@@ -70,8 +48,7 @@ class ZulipLogHandler(logging.Handler):
 
 class LogStream(PluginCommandMixin, Plugin):
     """
-    Manage user groups.
-    Alternative to Zulip user groups, as the bot does not have access to the api.
+    Send log messages to a Zulip stream.
     """
 
     def _init_plugin(self) -> None:
@@ -93,24 +70,3 @@ class LogStream(PluginCommandMixin, Plugin):
                 return
         
         logging.warning(f"Stream {logstram} not found")
-
-
-
-    @command
-    async def test(
-        self,
-        sender,
-        _session,
-        args: CommandParser.Args,
-        _opts,
-        _message: dict[str, Any],
-    ) -> AsyncGenerator[Response, None]:
-        """
-        Set the stream to log to.
-        """
-        text = await UserInput.short_text(self.client, sender, "What is your first name?")
-        if text is None:
-            yield Response.none()
-            return
-        
-        yield DMResponse(f"Hello {text}!")
